345da53e534a978766185a1bebb09ae9abaf8503
jimciccio <56683352+jimciccio@users.noreply.github.com>
2022-02-25 10:43:08 +0100

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,12 @@
 module ferranti.ciao {
+    requires transitive javafx.graphics;
     requires javafx.controls;
     requires javafx.fxml;
-    requires junit;
-    requires org.junit.jupiter.api;
+    requires java.sql;
+
+
     opens ferranti.ciao to javafx.fxml;
+    opens ferranti.ciao.models to javafx.base;
+    opens ferranti.ciao.controllers to javafx.fxml;
     exports ferranti.ciao;
 }
